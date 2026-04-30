1f42f17e249869aa46ec0926034d27d31d244843
Bernardo Bahia Monteiro <bbahia@umich.edu>
2026-04-30 17:55:32 -0500

    Add Netlib/specfun library

---
+++ modules/nwtc-library/src/NetLib/specfun/index.html.f
@@ -0,0 +1,155 @@
+<head>
+<title>specfun</title>
+<meta name="waisindex" value="nse">
+</head>
+<h1>specfun</h1>
+<p>
+Click <A HREF="http://www.netlib.org/master_counts2.html#specfun">here</A> to see the number of accesses to this library.
+<p><hr>
+<pre>
+file	<a href="readme">readme</a>
+for	overview of specfun
+
+file	<a href="algama">algama</a>
+for	log(gamma) function
+gams    c7a
+lang    fortran
+
+file	<a href="algtst">algtst</a>
+for	test program for log(gamma) function
+
+file	<a href="daw">daw</a>
+for	Dawson's integral
+gams    c8c
+lang    fortran
+
+file	<a href="dawtst">dawtst</a>
+for	test program for Dawson's integral
+
+file	<a href="ei">ei</a>
+for	various exponential integrals
+lang	fortran
+gams	c5
+
+file	<a href="eitest">eitest</a>
+for	test program for various exponential integrals
+
+file	<a href="erf">erf</a>
+for	error and scaled and unscaled complementary error functions
+gams    c8a
+lang    fortran
+
+file	<a href="erftst">erftst</a>
+for	test program for error and scaled and unscaled complementary error functions
+
+file	<a href="gamma">gamma</a>
+for	gamma function
+gams    c7a
+lang    fortran
+
+file	<a href="gamtst">gamtst</a>
+for	test program for gamma function
+
+file	<a href="i0">i0</a>
+for	Bessel function I-sub-0
+gams    c10b1
+lang    fortran
+
+file	<a href="i0test">i0test</a>
+for	test program for Bessel function I-sub-0
+
+file	<a href="i1">i1</a>
+for	Bessel function I-sub-1
+gams    c10b1
+lang    fortran
+
+file	<a href="i1test">i1test</a>
+for	test program for Bessel function I-sub-1
+
+file	<a href="j0test">j0test</a>
+for	test program for Bessel function J-sub-0
+
+file	<a href="j0y0">j0y0</a>
+for	Bessel functions J-sub-0 and Y-sub-0
+gams    c10a1
+lang    fortran
+
+file	<a href="j1test">j1test</a>
+for	test program for Bessel function J-sub-1
+
+file	<a href="j1y1">j1y1</a>
+for	Bessel functions J-sub-1 and Y-sub-1
+gams    c10a1
+lang    fortran
+
+file	<a href="k0">k0</a>
+for	Bessel function K-sub-0
+gams    c10b1
+lang    fortran
+
+file	<a href="k0test">k0test</a>
+for	test program for Bessel function K-sub-0
+
+file	<a href="k1">k1</a>
+for	Bessel function K-sub-1
+gams    c10b1
+lang    fortran
+
+file	<a href="k1test">k1test</a>
+for	test program for Bessel function K-sub-1
+
+file	<a href="newmach">newmach</a>
+for	environmental inquiry program
+
+file	<a href="psi">psi</a>
+for	the psi function
+gams    c7c
+lang    fortran
+
+file	<a href="psitst">psitst</a>
+for	test program for the psi function
+
+file	<a href="ren">ren</a>
+for	random number generator
+
+file	<a href="ribesl">ribesl</a>
+for	Bessel function I-sub-nu
+gams    c10b3
+lang    fortran
+
+file	<a href="ritest">ritest</a>
+for	test program for Bessel function I-sub-nu
+
+file	<a href="rjbesl">rjbesl</a>
+for	Bessel function J-sub-nu
+gams    c10a3
+lang    fortran
+
+file	<a href="rjtest">rjtest</a>
+for	test program for Bessel function J-sub-nu
+
+file	<a href="rkbesl">rkbesl</a>
+for	Bessel function K-sub-nu
+gams    c10b3
+lang    fortran
+
+file	<a href="rktest">rktest</a>
+for	test program for Bessel function K-sub-nu
+
+file	<a href="rybesl">rybesl</a>
+for	Bessel function Y-sub-nu
+gams    c10a3
+lang    fortran
+
+file	<a href="rytest">rytest</a>
+for	test program for Bessel function Y-sub-nu
+
+file	<a href="y0test">y0test</a>
+for	test program for Bessel function Y-sub-0
+
+file	<a href="y1test">y1test</a>
+for	test program for Bessel function Y-sub-1
+
+</pre>
+</body>
+</html>
